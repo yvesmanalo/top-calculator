259e44485a42ed780be8f9da4fb9c22503497d2f
yvesmanalo <23yvesmanalo@gmail.com>
2026-04-09 21:28:55 +0800

    Refactor displaying calculation result and added remaining button functionality

--- script.js
+++ script.js
@@ -7,15 +7,56 @@ const clearBtn = document.querySelector("#clear-btn");
 const display = document.querySelector("#display");
 const num1btn = document.querySelector("#one");
 const num2btn = document.querySelector("#two");
+const num3btn = document.querySelector("#three");
+const num4btn = document.querySelector("#four");
+const num5btn = document.querySelector("#five");
+const num6btn = document.querySelector("#six");
+const num7btn = document.querySelector("#seven");
+const num8btn = document.querySelector("#eight");
+const num9btn = document.querySelector("#nine");
+const num0btn = document.querySelector("#zero");
 
-let previousNumber = Number(null);
-let currentNumber = Number(null);
-let currentOperator = null;
+let prevNumber = Number(null);
+let currNumber = Number(null);
+let currOperator = String(null);
 let shouldOverwriteCurrentDisplay = true;
-let previousInputValue = null;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 !== 0) {
+    return num1 / num2;
+  } else {
+    return "Cannot divide by zero";
+  }
+}
+
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    return add(num1, num2);
+  }
+  if (operator === "-") {
+    return subtract(num1, num2);
+  }
+  if (operator === "*") {
+    return multiply(num1, num2);
+  }
+  if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
 
 num1btn.addEventListener("click", () => {
-  previousInputValue = 1;
   if (shouldOverwriteCurrentDisplay) {
     display.textContent = "1";
     shouldOverwriteCurrentDisplay = false;
@@ -25,7 +66,6 @@ num1btn.addEventListener("click", () => {
 });
 
 num2btn.addEventListener("click", () => {
-  previousInputValue = 2;
   if (shouldOverwriteCurrentDisplay) {
     display.textContent = "2";
     shouldOverwriteCurrentDisplay = false;
@@ -34,32 +74,146 @@ num2btn.addEventListener("click", () => {
   }
 });
 
+num3btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "3";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "3";
+  }
+});
+
+num4btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "4";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "4";
+  }
+});
+
+num5btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "5";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "5";
+  }
+});
+
+num6btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "6";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "6";
+  }
+});
+
+num7btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "7";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "7";
+  }
+});
+
+num8btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "8";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "8";
+  }
+});
+
+num9btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "9";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "9";
+  }
+});
+
+num0btn.addEventListener("click", () => {
+  if (shouldOverwriteCurrentDisplay) {
+    display.textContent = "0";
+    shouldOverwriteCurrentDisplay = false;
+  } else {
+    display.textContent += "0";
+  }
+});
+
 addBtn.addEventListener("click", () => {
   shouldOverwriteCurrentDisplay = true;
-  if (previousInputValue === "+") {
-    return;
+
+  if (!prevNumber) {
+    prevNumber = Number(display.textContent);
+  } else {
+    currNumber = Number(display.textContent);
+    prevNumber = operate(prevNumber, currNumber, currOperator);
+    display.textContent = `${prevNumber}`;
   }
-  currentNumber = Number(display.textContent);
-  previousNumber += currentNumber;
-  display.textContent = `${previousNumber}`;
-  currentOperator = "+";
-  previousInputValue = currentOperator;
+
+  currOperator = "+";
 });
 
 subtractBtn.addEventListener("click", () => {
   shouldOverwriteCurrentDisplay = true;
-  if (previousInputValue === "-") {
-    return;
-  }
-  if (previousNumber === 0) {
-    currentNumber = Number(display.textContent);
-    previousNumber = currentNumber;
-    display.textContent = `${currentNumber}`;
+
+  if (!prevNumber) {
+    prevNumber = Number(display.textContent);
   } else {
-    currentNumber = Number(display.textContent);
-    previousNumber -= currentNumber;
-    display.textContent = `${previousNumber}`;
+    currNumber = Number(display.textContent);
+    console.log(prevNumber, currNumber, currOperator);
+    display.textContent = `${prevNumber}`;
   }
-  currentOperator = "-";
-  previousInputValue = currentOperator;
+
+  currOperator = "-";
+});
+
+multiplyBtn.addEventListener("click", () => {
+  shouldOverwriteCurrentDisplay = true;
+
+  if (!prevNumber) {
+    prevNumber = Number(display.textContent);
+  } else {
+    currNumber = Number(display.textContent);
+    prevNumber = operate(prevNumber, currNumber, currOperator);
+    display.textContent = `${prevNumber}`;
+  }
+
+  currOperator = "*";
+});
+
+divideBtn.addEventListener("click", () => {
+  shouldOverwriteCurrentDisplay = true;
+
+  if (!prevNumber) {
+    prevNumber = Number(display.textContent);
+  } else {
+    currNumber = Number(display.textContent);
+    console.log(prevNumber, currNumber, currOperator);
+    prevNumber = operate(prevNumber, currNumber, currOperator);
+    display.textContent = `${prevNumber}`;
+  }
+
+  currOperator = "/";
+});
+
+equalBtn.addEventListener("click", () => {
+  shouldOverwriteCurrentDisplay = true;
+  currNumber = Number(display.textContent);
+  prevNumber = operate(prevNumber, currNumber, currOperator);
+  display.textContent = `${prevNumber}`;
+});
+
+clearBtn.addEventListener("click", () => {
+  prevNumber = Number(null);
+  currNumber = Number(null);
+  currOperator = String(null);
+  shouldOverwriteCurrentDisplay = true;
+  display.textContent = "";
 });
